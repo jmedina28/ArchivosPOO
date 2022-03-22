3f1cd4b0951e58e343b43cd9c3ddb768a8358bcd
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 19:09:38 +0100

    añadiendo cosas al main

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from gestiones import *
+
+
+def eleccion():
+    
+    variable = int(input("\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Lista de Aprobados.\n --> 3: Lista de suspensos.\n --> 4: Lista general.\n"))
+    if variable == 1:
+        listaaprobados()
+    elif variable == 2:
+        listasuspensos()
+    elif variable == 3:
+        
+    else:
+        print("Sólo son válidos los valores 1, 2 y 3.\n")
+        eleccion()
+eleccion()
