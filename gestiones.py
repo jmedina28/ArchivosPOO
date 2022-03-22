a9b037bb002a83effd57ab0403da24924a4383c1
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 16:36:45 +0100

    sigo probando y aprendiendo a manejar archivos csv

--- gestiones.py
+++ gestiones.py
@@ -1,5 +1,6 @@
-
+p = 0
 calificaciones = open("calificaciones.csv", "r")
-prueba = calificaciones.readline().split(";")
-
-print(prueba)
+prueba = calificaciones.readline()
+for i in range(16):
+    prueba = calificaciones.readline().split(";")
+    print(prueba)
