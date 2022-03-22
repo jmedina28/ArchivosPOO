2ab715aadc7c3a6336039407c06a138036992033
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 19:12:13 +0100

    arreglo del main

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from gestiones import *
 
 def eleccion():
     
-    variable = int(input("\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Lista de Aprobados.\n --> 3: Lista de suspensos.\n --> 4: Lista general.\n"))
+    variable = int(input("\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Lista de Aprobados.\n --> 2: Lista de suspensos.\n --> 3: Lista general.\n"))
     if variable == 1:
         listaaprobados()
     elif variable == 2:
         listasuspensos()
     elif variable == 3:
-        
+        listageneral()
     else:
         print("Sólo son válidos los valores 1, 2 y 3.\n")
         eleccion()
