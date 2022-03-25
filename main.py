842076acea2c18c336110735229ace79784ac818
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-25 13:23:19 +0100

    actualizo el main

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from gestiones import *
 
 def eleccion():
     
-    variable = int(input("\nPor favor, introduzca qué ejercicio desea realizar: \n --> 1: Lista de Aprobados.\n --> 2: Lista de suspensos.\n --> 3: Lista general.\n"))
+    variable = int(input("\nPor favor, introduzca qué desea ejecutar: \n --> 1: Lista de Aprobados.\n --> 2: Lista de suspensos.\n --> 3: Lista general.\n --> 4: Tabla.\n"))
     if variable == 1:
         listaaprobados()
     elif variable == 2:
         listasuspensos()
     elif variable == 3:
         listageneral()
+    elif variable == 4:
+        tabla()
     else:
-        print("Sólo son válidos los valores 1, 2 y 3.\n")
+        print("Sólo son válidos los valores 1, 2, 3 y 4.\n")
         eleccion()
 eleccion()
